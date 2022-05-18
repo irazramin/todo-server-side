7a406ba386fc4b38bc18d4310acda427f79f774f
irazramin <irazramin@gmail.com>
2022-05-18 23:36:33 +0600

    mongo setup

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const ObjectId = require('mongodb').ObjectId;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.kjpmx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  await client.connect();
+  const taskCollection = client.db('todoDb').collection('taskCollection');
+
+  try {
+   
+  } finally {
+  }
+};
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log('App is listening at ', port);
